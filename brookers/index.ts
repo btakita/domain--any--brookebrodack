318d722099fc4cad0b4084cdadbe00f184a80239
Brian Takita <brian.takita@gmail.com>
2024-02-05 15:22:20 -0500

    brookers__timeline_op_T→brookers_timeline_op_T

--- brookers/index.ts
+++ brookers/index.ts
@@ -1,3 +1,3 @@
-export type brookers__timeline_op_T =
+export type brookers_timeline_op_T =
 	|{ type:'html', title:string, bullet:'default'|'video', html:string }
 	|{ type:'youtube', title:string, bullet:'video', videoId:string }
