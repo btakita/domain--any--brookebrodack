c82caedd397c96783b02180176d6af6317aeddc5
Brian Takita <brian.takita@gmail.com>
2024-03-16 01:35:35 -0400

    ./social: + email_url

--- social/index.ts
+++ social/index.ts
@@ -1,3 +1,4 @@
+export const email_url = 'mailto:info@brookebrodack.net'
 export const instagram_url = 'https://www.instagram.com/brookebrodack'
 export const linkedin_url = 'https://www.linkedin.com/in/brooke-brodack-2223427'
 export const patreon_url = 'https://www.patreon.com/BrookeBrodack'
